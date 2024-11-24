1f2ab3f4854341f111d300f2f3c4a8db4c0ffe3b
SupNessy <162223896+SupNessy@users.noreply.github.com>
2024-11-24 15:05:51 -0500

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,6 +1,18 @@
-fetch ("https://www.googleapis.com/books/v1/volumes?q=modern%20web")
-    .then(res => res.json())
-    .then(console.log) 
-
+fetch ('google-books-search.json')
+    .then (res => res.json())
+    .then (data => {
+        const userInfoDiv = document.getElementById('info');
+        userInfoDiv.innerHTML = `        
+            <p> Title: ${data.title}</p>
+            <p> SubTitle: ${data.subtitle}</p>
+            <p> Authors: ${data.authors}</p>
+            <p> Publishers: ${data.publisher}</p>
+            <p> Description: ${data.description}</p>
+            <p> PageCount: ${data.pageCount}</p>
+            <p> Categories: ${data.categories}</p>
+            <p> Average Rating: ${data.averageRating}</p>
+            <p> Maturity Rating: ${data.maturityRating}</p>
+        `;
+    })
     .catch(error => console.error('error', error));
 ;
